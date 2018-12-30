f0dfe26f8e9a8fbaf1a009b5fc86a266de9e5e8f
Lohith013 <45910817+Lohith013@users.noreply.github.com>
2018-12-30 19:55:36 +0530

    bank app

---
+++ ConsoleApp4/Bank.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Bank : IRBI
+    {
+        public void Deposit(int acNo, decimal amt)
+        {
+           foreach(AccountHolder eachaccount in AccountHoldersList)
+            {
+                if (eachaccount.AccountId == acNo)
+                {
+                    eachaccount.Balance += amt;
+                }
+
+            }
+        }
+
+        public void FundTransfer(IRBI b, int acNo1, int acNo2, decimal amt)
+        {
+            foreach(AccountHolder account in AccountHoldersList)
+            {
+                if(account.AccountId == acNo1)
+                {
+                    account.Balance -= amt;
+                }
+            }
+
+            foreach(AccountHolder account in AccountHoldersList)
+            {
+                if(account.AccountId== acNo2)
+                {
+                    account.Balance += amt;
+                   
+                }
+            }
+            
+        }
+
+        public void Withdraw(int acNo, decimal amt)
+        {
+            foreach(AccountHolder account in AccountHoldersList)
+            {
+                if( account.AccountId == acNo)
+                {
+                    account.Balance -= amt;
+                }
+            }
+        }
+
+        public List<AccountHolder> AccountHoldersList = new List<AccountHolder>();
+
+        public void AddAccountHolder(AccountHolder newaccountholderinfo)
+        {
+       
+            AccountHoldersList.Add(newaccountholderinfo);
+
+        }
+        
+    }
+    
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            Bank test = new Bank();
+
+
+            /*             ------ create an new account-------------------------------             */
+            AccountHolder accountInfo = new AccountHolder();
+            Console.WriteLine("Enter the Account Holder Name");
+            accountInfo.Name = Console.ReadLine();
+            Console.WriteLine("Enter the Account Holder Address");
+            accountInfo.Address = Console.ReadLine();
+            Console.WriteLine("Enter the Account Number");
+            accountInfo.AccountId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Account Type");
+            accountInfo.AccountType = Console.ReadLine();
+           // Console.WriteLine("Enter the Account Balance");
+           // accountInfo.Balance = Convert.ToDecimal(Console.ReadLine());
+
+            test.AddAccountHolder(accountInfo);
+            //Console.WriteLine(test.AccountHoldersList.Count);
+            //Console.WriteLine(test.AccountHoldersList[0].Balance);
+            /*            ---------------------------- create an new account      ends here--------------------------------------------            */
+
+
+
+            /*            ---------------------------- deposit starts here--------------------------------------------            */
+            Console.WriteLine("Enter the account number");
+            int acNo = Convert.ToInt32(Console.ReadLine());
+               
+            Console.WriteLine("enter the amount to be deposited");
+            decimal amount = Convert.ToDecimal(Console.ReadLine());
+            test.Deposit(acNo, amount);
+
+
+
+            /*            ---------------------------- deposit   ends here--------------------------------------------            */
+
+
+
+            /*----------------------------Withdraw starts here----------------*/
+            test.Withdraw(acNo, amount);
+
+            /*----------------------------Withdraw ends here here----------------*/
+
+
+            /*----------------------Fund Transfer starts here----------------------------------*/
+            Sbi sbiobj = new Sbi();
+
+            Console.WriteLine("enter the bank");
+            string bank = Console.ReadLine();
+            Console.WriteLine("enter the account number from which money to transfered");
+            int AccNo1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the account number which receives amount");
+            int AccNo2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the amount to be transfered");
+            decimal transfreAmount = Convert.ToDecimal(Console.ReadLine());
+            test.FundTransfer(sbiobj, AccNo1, AccNo2, transfreAmount);
+
+            /*----------------------Fund Transfer ends here----------------------------------*/
+
+        }
+
+    }
+}
